eda04752f631da7bcad23562c870e3840cfdfa57
Oles <olestourko@gmail.com>
2018-05-07 21:26:19 -0400

    Follow Stackoverflow pagination, with optional limit

--- src/spiders/stackoverflow.py
+++ src/spiders/stackoverflow.py
@@ -8,11 +8,26 @@ class StackOverflowSpider(scrapy.Spider):
         'https://stackoverflow.com/jobs?med=site-ui&ref=jobs-tab&sort=p'
     ]
 
+    def __init__(self, max_pages=None):
+        self.crawled_pages = 0
+        self.max_pages = int(max_pages)
+        super(StackOverflowSpider, self)
+
     def parse(self, response):
+        self.crawled_pages += 1
+        print(self.crawled_pages)
+        base_url = 'https://stackoverflow.com'
+
         for job in response.css('.list.jobs .-job'):
             route = job.css('.-job-summary a::attr("href")').extract_first()
-            url = urljoin('https://stackoverflow.com', route)
+            url = urljoin(base_url, route)
             yield scrapy.Request(url=url, callback=self.parse_job_post_page)
+
+        # Go to the next page
+        if self.max_pages is None or self.crawled_pages < self.max_pages:
+            next_page_url = response.css('.pagination .test-pagination-next::attr("href")').extract_first()
+            if next_page_url:
+                yield response.follow(url=next_page_url, callback=self.parse)
 
 
     def parse_job_post_page(self, response):
